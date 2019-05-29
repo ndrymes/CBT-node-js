4b5980856628442cf9c837fc32a1cd6e788c1d4b
Sunmonu Oluwole <sunmonuoluwole@gmail.com>
2019-05-29 15:33:59 +0100

    setup

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const hbs = require('hbs')
-
+const port=process.env.PORT || 3000
 const app =express()
 app.set('view engine','hbs')
 
@@ -9,6 +9,6 @@ app.use(express.static(__dirname +'/static/css'))
  app.get('',(req,res) => {
    res.render('test.hbs',{})
  })
-app.listen(3000,(report)=> {
+app.listen(port,(report)=> {
   console.log('server is up on port 3000');
 })
